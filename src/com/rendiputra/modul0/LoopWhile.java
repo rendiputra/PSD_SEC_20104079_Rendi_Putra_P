39f7541213948509710fee4c76a0e2c62862dd39
rendiputra <rendiputrapradana@gmail.com>
2021-04-07 10:26:45 +0700

    updated LoopWhile

--- src/com/rendiputra/modul0/LoopWhile.java
+++ src/com/rendiputra/modul0/LoopWhile.java
@@ -1,4 +1,18 @@
 package com.rendiputra.modul0;
 
+import java.util.Scanner;
+
 public class LoopWhile {
+    public static void main(String[] args) {
+        System.out.print("Masukan angka : ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        while (a % 2 == 0) {
+            System.out.println("Ini bilangan genap(Ulang lagi)");
+            System.out.print("Masukan angka : ");
+            a = scan.nextInt();
+        }
+
+    }
 }
