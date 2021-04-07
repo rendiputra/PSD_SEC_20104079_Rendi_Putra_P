1e207c3fcb9dcbf73822282bc8bb9d67cf2c652a
rendiputra <rendiputrapradana@gmail.com>
2021-04-07 10:29:05 +0700

    updated LoopFor

--- src/com/rendiputra/modul0/LoopFor.java
+++ src/com/rendiputra/modul0/LoopFor.java
@@ -1,4 +1,19 @@
 package com.rendiputra.modul0;
 
+import java.util.Scanner;
+
 public class LoopFor {
+    public static void main(String[] args) {
+//        mencetak statment menggunakan looping
+
+        System.out.print("Masukan loop akhir : ");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        for (int i = 1; i <=a; i++){
+            System.out.print(i +"  ");
+        }
+
+
+    }
 }
