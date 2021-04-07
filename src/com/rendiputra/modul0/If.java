5967c674289c62b8e4148b328d1f1c7555e618d4
rendiputra <rendiputrapradana@gmail.com>
2021-04-07 10:11:08 +0700

    updated If

--- src/com/rendiputra/modul0/If.java
+++ src/com/rendiputra/modul0/If.java
@@ -1,4 +1,20 @@
 package com.rendiputra.modul0;
 
+import java.util.Scanner;
+
 public class If {
+    public static void main(String[] args) {
+        int nilai;
+
+        Scanner scan = new Scanner( (System.in));
+        nilai = scan.nextInt();
+
+        if(nilai>0){
+            System.out.println("NIlai "+nilai+ " merupakan bilangan bulat positif");
+        } else if(nilai<0){
+            System.out.println("NIlai "+nilai+ " merupakan bilangan bulat negatif");
+        } else {
+            System.out.println("NIlai "+nilai+ " merupakan bilangan cacah");
+        }
+    }
 }
