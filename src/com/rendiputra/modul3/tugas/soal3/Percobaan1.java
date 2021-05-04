7c0d1639f117b15539d1de4691f8f4b6a53d6c9f
rendiputra <rendiputrapradana@gmail.com>
2021-05-05 02:19:46 +0700

    modul3(soal 3): modified code to calculate runtime to find fibonnaci

---
+++ src/com/rendiputra/modul3/tugas/soal3/Percobaan1.java
@@ -0,0 +1,34 @@
+package com.rendiputra.modul3.tugas.soal3;
+
+public class Percobaan1 {
+    public static long fib1(long Num){
+        if (Num == 0)
+            return 0;
+        else if (Num == 1)
+            return 1;
+        else{
+            long x = 0;
+            long y = 1;
+            for(long k =2; k<=Num; k++){
+                y = x + y;
+                x = y - x;
+            }
+            return y;
+        }
+    }
+
+    public static void main(String[] args) {
+        //Start time
+        long begin = System.currentTimeMillis();
+
+        //Starting the watch
+        long bil = fib1(10);
+        System.out.println(bil);
+
+        //End time
+        long end = System.currentTimeMillis();
+        long time = end-begin;
+        System.out.println();
+        System.out.println("Elapsed Time: "+time +" milli seconds");
+    }
+}
