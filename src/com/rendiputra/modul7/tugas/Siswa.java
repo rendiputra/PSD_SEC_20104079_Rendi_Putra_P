03c32fbe4b4d8f3aa4cafa3ebb02e0147913c462
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-06-28 18:10:45 +0700

    modul7: add file "tugas/Siswa.java"

---
+++ src/com/rendiputra/modul7/tugas/Siswa.java
@@ -0,0 +1,13 @@
+package com.rendiputra.modul7.tugas;
+
+public class Siswa {
+    int NIM;
+    String nama;
+    String alamat;
+
+    public Siswa (int NIM, String nama, String alamat){
+        this.NIM = NIM;
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+}
