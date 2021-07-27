066f6289b6e3d14e165c686df53bddf2e9684ff1
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-07-28 01:31:49 +0700

    modul11: Create file "QuickSort.java"

---
+++ src/com/rendiputra/modul11/praktikum/QuickSort.java
@@ -0,0 +1,40 @@
+package com.rendiputra.modul11.praktikum;
+
+public class QuickSort {
+    public static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for(int j = low; j <= high - 1; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+    public static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    public static void printArray(int[] arr, int size)
+    {
+        for(int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
